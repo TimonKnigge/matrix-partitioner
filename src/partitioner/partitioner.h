53672fdbd17f9ed29a8f27a2289c74cb559c2797
Timon Knigge <timonknigge@live.nl>
2017-07-06 21:18:38 +0200

    Allow epsilon imbalance.

--- src/partitioner/partitioner.h
+++ src/partitioner/partitioner.h
@@ -15,7 +15,7 @@ class partitioner {
 	// Partition a given matrix, and store the assignment in row and col.
 	// Returns true upon succes, false upon failure.
 	virtual bool partition(const matrix &m, std::vector<status> &row,
-		std::vector<status> &col, std::clock_t clocks) = 0;
+		std::vector<status> &col, float epsilon, std::clock_t clocks) = 0;
 };
 
 }

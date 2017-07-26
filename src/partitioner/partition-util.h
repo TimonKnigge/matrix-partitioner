ee64724da23cfaa0cf63e8d4a8a5c66182020162
Timon Knigge <timonknigge@live.nl>
2017-07-26 15:15:10 +0200

    Add utility functions for statuses.

--- src/partitioner/partition-util.h
+++ src/partitioner/partition-util.h
@@ -5,10 +5,46 @@
 
 namespace mp {
 
+constexpr int RED = 0, BLUE = 1;
 enum status {
-	red = 0, blue = 1, cut = 2, unassigned = 3
+	red = 0, blue = 1, cut = 2, unassigned = 3,
+	partial_red = 4, partial_blue = 5, implicitly_cut = 6
 };
 
+inline status to_partial(status s) {
+	if (s == status::red) return status::partial_red;
+	if (s == status::blue) return status::partial_blue;
+	return s;
+}
+
+inline status from_partial(status s) {
+	if (s == status::partial_red) return status::red;
+	if (s == status::partial_blue) return status::blue;
+	return s;
+}
+
+inline bool is_partial(status s) {
+	return s == status::partial_red || s == status::partial_blue;
+}
+
+inline status color_swap(status s) {
+	if (s == status::red) return status::blue;
+	if (s == status::blue) return status::red;
+	if (s == status::partial_red) return status::partial_blue;
+	if (s == status::partial_blue) return status::partial_red;
+	return s;
+}
+
+inline int get_color(status s) {
+	return s == status::red || s == status::partial_red ? RED : BLUE;
+}
+
+inline status color_to_status(int c) {
+	return c == 0 ? status::red : status::blue;
+}
+
+inline status color_to_partial_status(int c) {
+	return c == 0 ? status::partial_red : status::partial_blue;
 }
 
 #endif

ba99d73b7d3142bc7577d49106194f74bfef653c
Timon Knigge <t.e.knigge@gmail.com>
2018-05-23 21:32:50 +0200

    Readd improved packing set.

---
+++ src/datastructures/packing-set.cpp
@@ -0,0 +1,41 @@
+#include "./packing-set.h"
+
+#include <algorithm>
+
+namespace mp {
+
+void packing_set::add(int c) {
+	values[c]++;
+	ps_size = -1;
+}
+
+void packing_set::remove(int c) {
+	auto it = values.find(c);
+	it->second--;
+	if (it->second == 0)
+		values.erase(it);
+	ps_size = -1;
+}
+
+void packing_set::set_lower_bound(int nC) {
+	C = nC;
+	ps_size = -1;
+}
+
+int packing_set::get_minimum_packing_set_size() const {
+	if (ps_size == -1)
+		recompute();
+	return ps_size;
+}
+
+void packing_set::recompute() {
+	int ps = 0;
+	ps_size = 0;
+	for (auto it = values.rbegin(); ps < C && it != values.rend(); ++it) {
+		int use = std::min(it->second, (C - ps + it->first - 1) / it->first);
+		ps_size += use;
+		ps += use * it->first;
+	}
+}
+
+}
---
+++ src/datastructures/packing-set.h
@@ -0,0 +1,35 @@
+#ifndef PACKING_SET_H
+#define PACKING_SET_H
+
+#include <map>
+
+namespace mp {
+
+// A datastructure for dynamically maintaining a 'packing set'.
+// Specifically, this datastructure maintains a multiset of integers
+// as well as a lowerbound C, and can return the size of the smallest
+// subset of the multiset that sums to >= C.
+
+class packing_set {
+
+public:
+	// Add/remove integers.
+	void add(int c);
+	void remove(int c);
+	
+	// Modify the lowerbound.
+	void set_lower_bound(int nC);
+
+	// Retrieve the size of the packing set.
+	int get_minimum_packing_set_size() const;
+
+private:
+	std::map<int, int> values;
+	int C = 0, ps_size = -1;
+
+	void recompute();
+};
+
+}
+
+#endif

f92e4a82cf77a33972642e8994e4b9f39c4a0dd2
Timon Knigge <t.e.knigge@gmail.com>
2018-06-12 22:52:55 +0200

    Update the parameters for B&B a bit.

--- src/bb/bb-parameters.cpp
+++ src/bb/bb-parameters.cpp
@@ -3,8 +3,10 @@
 namespace mp {
 
 std::pair<bool, std::string> bbparameters::valid() const {
-	if (pb && epb) return {false, "Can only use one packing bound."};
+	if (mb) return {false, "Matching bound not implemented."};
 	if (mb && fb) return {false, "Can only use one flow bound."};
+	if (epb && !fb) return {false, "Extended packing bound must be "
+		"used in combination with the flow bound."};
 	return {true, ""};
 }
 

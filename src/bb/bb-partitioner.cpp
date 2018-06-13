TimonKnigge/matrix-partitioner

commit 8eaa26db0e75d3594f21ea7679d415f48d51dbb8

Add dedicated 'pick next' method.

src/bb/bb-partitioner.cpp
@@ -71,6 +71,27 @@ void bbpartitioner::recurse(int rc, status stat,
 			rc, stat});
 }
 
+bool bbpartitioner::pick_next(size_t &current_rcs, std::vector<int> &rcs,
+		partial_partition &pp, int lower_bound, int upper_bound) {
+	if (current_rcs == rcs.size() || lower_bound >= upper_bound)
+		return false;
+
+	// First we branch on implicitly cut vertices, if they exist. Otherwise
+	// we pick the vertex with the largest degree.
+	for (size_t i = current_rcs; i < rcs.size(); ++i) {
+		if (pp.get_status(rcs[i]) == status::implicitly_cut) {
+			std::swap(rcs[current_rcs], rcs[i]);
+			break;
+		}
+		if (pp.get_free_nonzeros(rcs[i])
+				> pp.get_free_nonzeros(rcs[current_rcs])) {
+			std::swap(rcs[current_rcs], rcs[i]);
+		}
+	}
+
+	return true;
+}
+
 int bbpartitioner::make_step(std::stack<recursion_step> &call_stack,
 		size_t &current_rcs, std::vector<int> &rcs, partial_partition &pp,
 		int upper_bound) {
@@ -81,25 +102,16 @@ int bbpartitioner::make_step(std::stack<recursion_step> &call_stack,
 	if (step.rt == recursion_type::descend) {
 		lb = pp.assign(step.rc, step.s, upper_bound);
 
-		// Branch again.
+		// Try branching again.
 		++current_rcs;
-		if (current_rcs < rcs.size() && lb < upper_bound) {
-			// We consider what vertex to branch on. The candidates are
-			// rcs[current_rcs, ..). 
-			// Strategy:
-			//	- Prioritize implicitly cut
-			//	- We pick the one that has the most free nonzeros.
-			for (size_t i = current_rcs + 1; i < rcs.size(); ++i) {
-				if (pp.get_free_nonzeros(rcs[current_rcs])
-						< pp.get_free_nonzeros(rcs[i])
-						|| (pp.get_status(rcs[i]) == status::implicitly_cut
-							&& pp.get_status(rcs[current_rcs]) != status::implicitly_cut))
-					std::swap(rcs[current_rcs], rcs[i]);
-			}
-
+		if (pick_next(current_rcs, rcs, pp, lb, upper_bound)) {
 			// Recurse on the cut last (note that branches are executed on a
-			// stack and thus in reverse order).
-			recurse(rcs[current_rcs], status::cut, call_stack, pp);
+			// stack and thus in reverse order). Also, if lb + 1 == ub and this
+			// vertex is NOT implicitly cut, there is no need to branch on
+			// the cut.
+			if (pp.get_status(rcs[current_rcs]) == mp::status::implicitly_cut
+					|| lb + 1 < upper_bound)
+				recurse(rcs[current_rcs], status::cut, call_stack, pp);
 
 			// First branch on the smaller component.
 			if (pp.get_partition_size(0) > pp.get_partition_size(1)) {

src/bb/bb-partitioner.h
@@ -35,6 +35,11 @@ class bbpartitioner : public partitioner {
 	void recurse(int rc, status stat, std::stack<recursion_step> &call_stack,
 		const partial_partition &pp);
 
+	// Pick the next vertex to branch on. Just moves it into position
+	// rcs[current_rcs] so the algorithm will pick it up.
+	bool pick_next(size_t &current_rcs, std::vector<int> &rcs,
+		partial_partition &pp, int lower_bound, int upper_bound);
+
   public:
 	bbpartitioner(bbparameters _param) : param(_param) { }
 

commit 1fa7104f365bfd6d32a27c8dc8d578ed9d85f714

Fix bug introduced in 8eaa26db which overestimated volume lower bound.

src/bb/bb-partitioner.cpp
@@ -110,7 +110,7 @@ int bbpartitioner::make_step(std::stack<recursion_step> &call_stack,
 			// vertex is NOT implicitly cut, there is no need to branch on
 			// the cut.
 			if (pp.get_status(rcs[current_rcs]) == mp::status::implicitly_cut
-					|| lb + 1 < upper_bound)
+					|| pp.get_guaranteed_lower_bound() + 1 < upper_bound)
 				recurse(rcs[current_rcs], status::cut, call_stack, pp);
 
 			// First branch on the smaller component.

src/bb/partial-partition.cpp
@@ -467,4 +467,10 @@ int partial_partition::get_free_nonzeros(int rc) const {
 		- color_count[1][rc];
 }
 
+int partial_partition::get_guaranteed_lower_bound() const {
+	return cut;
+}
+
+
+
 }

src/bb/partial-partition.h
@@ -102,6 +102,9 @@ class partial_partition {
 	// How many free nonzeros there are in this row/column.
 	int get_free_nonzeros(int rc) const;
 
+	// How many actual rows/columns have been cut (no lowerbounding).
+	int get_guaranteed_lower_bound() const;
+
 	// Friend for debugging.
 	friend void print_ppmatrix(std::ostream &stream,
 		const partial_partition &pp);